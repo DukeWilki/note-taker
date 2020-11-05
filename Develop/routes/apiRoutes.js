b7ba4501b32fc06b77851ba240e525060ff3467b
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-11-05 22:33:05 +1100

    uuid function fixed

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,10 +8,8 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = function(app) {
 
+  // GET: READ NOTES FROM DB.JSON
   app.get('/api/notes', (req, res) => {
-    // retrieve all the notes from json
-    // res.json(notes);
-
 
   fs.readFile("db/db.json","utf8", (err, data) => {
     if (err) throw err;
@@ -22,33 +20,33 @@ module.exports = function(app) {
 })
   // })
   
-
+  // POST: CREATING NEW NOTES ON THE APPLICATION
   app.post('/api/notes', (req, res) => {
     // generate a uuid (google npm uuid)
     uuidv4(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
     // grab the req payload
     const title = req.body.title;
     const text = req.body.text;
+    const id = uuidv4()
 
     const newNote = {
-      id: uuidv4,
+      id,
       title: title,
       text: text,
     }
 
-    // const jsonData = fs.readFileSync('./../db/db.json', 'utf8');
     const jsonData = fs.readFileSync(__dirname + '/../db/db.json', 'utf8');
 
     const notes = JSON.parse(jsonData) || [];
 
+    // ADD NEW NOTE TO DB.JSON
     // create a new note in json
     notes.push(newNote)
-
-    // fs.writeFileSync('./../db/db.json', JSON.stringify(notes) )
     fs.writeFileSync(__dirname + '/../db/db.json', JSON.stringify(notes))
 
     res.json({
       data: notes,
+      id,
     })
 
   })
@@ -56,7 +54,7 @@ module.exports = function(app) {
   app.delete('/api/notes/:id', (req, res) => {
     // 
     //  req.param.id
-    const id = req.query.id;
+    // const id = req.query.id;
 
     // filter out the note that has the id passed in, 
     // resave the data to db.json
--- Develop/server.js
+++ Develop/server.js
@@ -8,13 +8,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());  
 
 // allow the server to serve up static files within the public folder
 app.use(express.static('public'));
 
-require("./routes/apiRoutes")(app);               // I swapped these lines around as the /api/notes get method was for some reason  returning index.html
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
